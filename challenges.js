cfda57a78204ac082a9425129b9bc443e4205fc2
Adam Endrodi <adam@ginf.hu>
2023-12-19 23:20:11 +0000

    initial checkin
    The sole purpose of this silly game is to entertain my family.  I put it
    together in haste (so the code is not up to my standards) and there are
    a lot of copyrighted content in the repo that I probably had no right to
    redistribute.  My only excuse is that I mean no harm, and may take it down
    altogether.

---
+++ challenges.js
@@ -0,0 +1,376 @@
+challenges =
+[
+	{
+		"topic": "IT Security",
+		"short": "ITSec",
+		"challenges":
+		[
+			[ "What does it mean when the lock icon is crossed in a browser?" ],
+			[ "What is browser fingerprinting?" ],
+			[
+				"What are VPNs used for?",
+				"Torrenting for example.",
+			],
+			[
+				"What is a man-in-the-middle attack?",
+				"Or \"person-in-the-middle\" to be polite.",
+			],
+			[
+				"What is port mapping?",
+				"What hackers are portrayed to be doing in movies.",
+			],
+			[
+				"What is a CAPTCHA?",
+				"Nowadays computers are better at them than humans.",
+				"Completely Automated Public Turing test to tell Computers and Humans Apart",
+			],
+			[
+				"What is DDoS?",
+				"Recent 300M+ QPS incident, which passed without user visibility.",
+			],
+		],
+	},
+	{
+		"topic": "Games",
+		"challenges":
+		[	// Prince?  Alley cat?  Eszkerek?
+			[
+				"Which game is this? (#1)",
+				[ "audio", "hints/Quasimodo.mp3" ],
+				[ "image", "hints/Quasimodo.png", "Screenshot" ],
+				"Hunchback",
+			],
+			[
+				"Which game is this? (#2)",
+				[ "audio", "hints/The_Great_Giana_Sisters.webm" ],
+				[ "image", "https://upload.wikimedia.org/wikipedia/en/9/90/Giana_Sisters_Screenshot.png", "Screenshot" ],
+				"Popular song, many remixes",
+				"Giana is having a nighmare full of monsters, looking for her sister Maria.",
+			],
+			[
+				"Which game is this? (#3)",
+				[ "audio", "hints/The_Lost_Vikings.mp3" ],
+				"Erik, Baleog, Olaf",
+				[ "image", "hints/The_Lost_Vikings.png", "Screenshot" ],
+			],
+			[
+				"Which game is this? (#4)",
+				[ "audio", "hints/Worms.webm" ],
+			],
+			[
+				"Which game is this? (#5)",
+				[ "audio", "hints/Revenge_on_School.mp3" ],
+				[ "image", "https://www.dosjatekok.hu/kepek/jatekkepek/suli.png", "Screenshot" ],
+				"Tomcat",
+			],
+			[
+				"Which game is this? (#6)",
+				[ "image", "https://www.dosjatekok.hu/kepek/jatekkepek/stoneage.png", "Screenshot" ],
+			],
+			[
+				"Which game is this? (#7)",
+				[ "image", "https://www.dosjatekok.hu/kepek/jatekkepek/prehistorik.png", "Screenshot" ],
+				"Prehistorik",
+				"It also had a sequel.",
+			],
+			[
+				"In which game could you invent the phalanx?",
+				"Civilisation IV. was released in 2016.",
+			],
+			[
+				"Which game did I call \"békás tapicskoló\"?",
+				[ "image", "https://www.myabandonware.com/media/screenshots/s/space-invaders-7mv/space-invaders_4.png", "Screenshot" ],
+			],
+			[
+				"What game is Reversi?",
+				[ "image", "https://www.dosjatekok.hu/kepek/jatekkepek/reversi.png", "Screenshot" ],
+			],
+			[
+				"What game is Soko-Ban?",
+				[ "image", "https://www.dosjatekok.hu/kepek/jatekkepek/skb.png", "Screenshot" ],
+			],
+			[ "In Monopoly how much did the police fine us for illegal crossing?" ],
+		],
+	},
+	{
+		"topic": "Movies",
+		"challenges":
+		[
+			[
+				"Name five characters from Pom Pom meséi.",
+				"Radírpók, Madárvédő Golyókapkodó, Festéktüsszentő Hapci Benő, Órarugó Gerincű Felpattanó, a Bátor Tintanyúl, a Lesbőltámadó Ruhaszárítókötél,  Ragasztó Rezső, a Szomorú Szamovár",
+			],
+			[
+				"Name five toy characters from Toy Story.",
+				"Buzz, Rex, Bo, Hamm, Slink, Potato, Woody, Etch, Wheezy, Jessie, Bullseye",
+			],
+			[
+				"Which weapon is not from Star Wars: light sabre, photon torpedo, proton torpedo",
+				"One of the weapons was used to destroy the first Death Star.",
+			],
+			[
+				"Which weapon is not from Star Trek: phaser, turbolaser, disruptor",
+				"Disruptors are employed by Klingons and Romulans.",
+				[ "image", "https://static.wikia.nocookie.net/starwars/images/a/ac/DSTurbolaser.png", "Turbolaser" ],
+			],
+			[
+				"What kind of droid is C3PO?",
+				"Fluent in over six million forms of communication.",
+			],
+			[
+				"What was the model of the shape-shifter Terminator?",
+				"T-something",
+			],
+			[
+				"In which movie did aliens send prime numbers to Earth?",
+				"Jodie Foster, best sci-fi movei I've ever seen.",
+			],
+			[
+				"Tell five quips from Macskafogó.",
+				"Cat City, 4 Gangsters",
+			],
+			[ "What was the cat's name in Mézga?" ],
+			[
+				"What was the name of the sergeant in Angyalbőrben?",
+				"Olajbogyó, Usztics Mátyás",
+				"Lots of explicit scenes, including an episode on a nude beach.",
+			],
+			[
+				"Where is this quote from: \"Ellensége magának az a gombóc, hogy így püföli?\"",
+				"Itt a gombóc, megvan a gombóc!  Gyere, gombóc, gyere!",
+			],
+			[
+				"Where is this quote from: \"Nem csak Travoltát hívják Johnny-nak!\"",
+				"When the driver reports to his boss that his truck has been stolen.",
+			],
+			[
+				"What was the name of the operator in the first Matrix movie?",
+				"The guy who could upload skills to the decked-in people, like flying a chopper.",
+			],
+			[
+				"In which movie could we hear this tune?",
+				[ "audio", "hints/when_Johnny_comes_marching_home.mp3" ],
+				"Terrorists planted bombs in NYC and stole all the gold from the Federal Reserve Bank.",
+			],
+			[ "Name six smurfs from Smurfs." ],
+			[
+				"Which story was Szaffi based on?",
+				"Jókai Mór: A cigánybáró",
+			],
+			[
+				"Where is this quote from: \"Nem vagy buta, csak keveset tudsz.\"",
+				"Ágyelő",
+			],
+			[ "Where is this quote from: \"Alacsonyan szállnak a stukkerek.\"" ],
+			[
+				"Which series' theme song is this? (#1)",
+				[ "audio", "hints/Game_of_Thrones.mp3" ],
+			],
+			[
+				"Which series' theme song is this? (#2)",
+				[ "audio", "hints/Twin_Peaks.mp3" ],
+			],
+			[
+				"In which children's tale were metallic spiders?",
+				[ "audio", "hints/gepkutya.mp3" ],
+				"Crazy robot on a deserted base on a distant planet.",
+			],
+			[ "In which movie did Sarah set out to rescue her baby brother from David Bowie?" ],
+		],
+	},
+	{
+		"topic": "Programming",
+		"short": "Coding",
+		"challenges":
+		[
+			[ "What is code archeology?" ],
+			[ "What is spaghetti code?" ],
+			[ "What is a script?" ],
+			[ "What is garbage collection?" ],
+			[ "What is a FIFO?" ],
+			[ "What does full outer join mean in SQL?" ],
+			[ "What does the REM command do in BASIC?" ],
+			[ "What is turtle graphics?" ],
+			[
+				"Why do programmers always mix up Halloween and Christmas?",
+				"Dec 25 == oct 31",
+			],
+		],
+	},
+	{
+		"topic": "IT History",
+		"short": "Lore",
+		"challenges":
+		[
+			[ "Since when is the Internet operating?" ],
+			[ "Name 5 current or historical web search engines." ],
+			[
+				"How much data could be stored on a floppy disk?",
+				[ "image", "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Floppy_disk_2009_G1.jpg/1024px-Floppy_disk_2009_G1.jpg", "Illustration" ],
+			],
+			[
+				"What is a streamer?",
+				[ "image", "https://upload.wikimedia.org/wikipedia/commons/3/39/Dds_tape_drive_01.jpg", "Illustration" ],
+				"Until recently Google used to store backups on tape.",
+				"Once the backups of GMP had to be restored from 1M tapes.",
+			],
+			[
+				"How much music could an audio CD store?",
+				"How would you given the capacity (650 MiB)?",
+				"74m == Beethoven's 9th Symphony",
+			],
+			[
+				"What was the killer feature of Windows 95?",
+				"Multitasking",
+			],
+			[
+				"What is a diskmag?",
+				"KenderMag",
+			],
+		],
+	},
+	{
+		"topic": "Google",
+		"challenges":
+		[
+			[ "Who is Larry Page?" ],
+			[
+				"What is the company named after?",
+				"A big number.",
+			],
+			[
+				"When was Google founded?",
+				"Which decade?",
+				"We've just celebrated a round anniversary.",
+			],
+			[ "What company is Alphabet Inc.?" ],
+			[ "What is site reliability engineering?" ],
+			[
+				"Name 5 Google products used by >1B people daily.",
+				[ "image", "hints/Google.png", "Icons" ],
+			],
+			[
+				"Where are the Google headquarters?",
+				"California",
+			],
+			[
+				"Name four discontinued Google products.",
+				"Picasa, Wave, Orkut, Google+, Knol",
+			],
+		],
+	},
+	{
+		"topic": "Geek stuff",
+		"short": "Geek",
+		"challenges":
+		[
+			[
+				"What's the Earth's distance from the Sun?",
+				"Also called the astronomical unit.",
+				"Light travels for 8m30s.",
+			],
+			[
+				"Which is the closest star to the Solar System?",
+				"It's also in its name.",
+				"Proxima Centauri, 4.2 ly",
+			],
+			[
+				"How much is a parsec?",
+				"How would you calculate it?",
+				"3.2 ly",
+			],
+			[
+				"What is the first letter of the Hebrew alphabet?",
+				"Aleph-0: the smallest infinite cardinal number",
+			],
+			[ "Is P = NP?" ],
+			[ "What is the demoscene?" ],
+			[ "If 1.5 hens lay 1.5 eggs in 1.5 days, how many eggs do 9 hens lay in 9 days?" ],
+			[
+				"How many nouns are in \"Buffalo buffalo buffalo buffalo buffalo buffalo buffalo buffalo\"?",
+				"This is also a deep psychological wisdom.",
+			],
+		],
+	},
+	{
+		"topic": "Generic IT",
+		"short": "IT",
+		"challenges":
+		[
+			[
+				"What's the difference between a gigabyte and a gibibyte?",
+				"GB vs. GiB",
+			],
+			[
+				"What is the largest number representable in a 32-bit integer?",
+				"Powers of 2",
+			],
+			[
+				"How many bits do you need to address one exabyte of storage?",
+				"Giga, tera, peta, exa",
+			],
+			[ "What is FTP?" ],
+			[ "What is hashing?" ],
+			[ "How does MP3 work?" ],
+			[
+				"What is blue death?",
+				[ "image", "https://upload.wikimedia.org/wikipedia/commons/6/61/Windows_NT_3.51_BSOD_ita.png", "Illustration" ],
+			],
+		],
+	},
+	{
+		"topic": "Literature",
+		"short": "Books",
+		"challenges":
+		[
+			[
+				"Name six books by István Nemere.",
+				"79 yo, 744 books",
+			],
+			[
+				"Who were the members of the Council of Four?",
+				"László Endre",
+			],
+			[
+				"What were the three slogans of the Party in 1984?",
+				"War is peace",
+				"Freedom is slavery",
+				"Ignorance is strength",
+			],
+			[ "What's the Third Law of Robotics according to Isaac Asimov?" ],
+			[
+				"What is psychohistory?",
+				"Asimov, Foundation",
+			],
+			[
+				"What was the name of Harry Potter's youngest son?",
+				"Revealed on the last page of the last novel.",
+				"He was named after two people who played a significant role in Harry's life.",
+			],
+			[
+				"Continue: \"Uram, a késemért jöttem!\"",
+				"Rejtő Jenő",
+			],
+			[
+				"How does the Wicked Witch die in The Wizard of Oz?",
+				[ "image", "https://upload.wikimedia.org/wikipedia/commons/b/bc/Wicked_Witch2.jpg", "Illustration" ],
+				"Reminescent of the destruction of the shape-shifter Terminator.",
+			],
+			[
+				"In the Twilight tetralogy what special skills do vampires possess?",
+				"Perfect physical features, strength, speed, senses, they don't need sleep, immortality, you name it.",
+				"Stephenie Meyer: The Host",
+			],
+			[
+				"In the Timothy Zahn universe who was the Emperor's Hand?",
+				"Later Mrs. Skywalker.",
+			],
+			[
+				"Which book is this poem from?",
+				"Kóbor kaffer kószál királylány kertjében / Királylány kacéran kacsint kéjvágyó kedvében / Kapj karodba, kaffer! Király kinéz, kiált / Katonák! Kürtszó, kivégzés. Királylány kacag kuszán / Kegyetlen kor! Kicsapongó, koronás kurtizán!",
+				"Dőre, aki magát okosabbnak hívén / Packázni akar az Elektrubadúrral / Szégyenletes kudarc lesz az osztályrésze / Íme, ez történik Klapanciusz úrral!",
+				"Írjon kiberotikus verset! Legfeljebb öt sor legyen, de szóljon szerelemről, árulásról és halálról, a néger kérdésről és a nimfomániáról, legyen benne a bonyolult női lélek extrém konfliktushelyzetben bekövetkező meghasonlásának ábrázolása, a középkori feudális viszonyok és erkölcsök maró bírálata, rímeljen, és minden szó k betűvel kezdődjön!",
+			],
+		],
+	}
+];
